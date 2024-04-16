307f0bb3b6ef017e76864c0bc005f5d842b3f39a
martinsobinna <martinsobinna9@gmail.com>
2024-04-16 22:20:46 +0000

    new task

--- 0-get_list_students.js
+++ 0-get_list_students.js
@@ -1,6 +1,7 @@
-export default function getListStudentIds(list) {
-  if (Array.isArray(list)) {
-    return list.map((obj) => obj.id);
-  }
-  return [];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
