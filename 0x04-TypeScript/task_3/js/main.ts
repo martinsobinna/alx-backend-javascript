9f480010202f4236b953f89768d0a6022ac64e8c
martinsobinna <martinsobinna9@gmail.com>
2024-04-17 23:16:19 +0000

    new tasks

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,15 @@
+import {RowID, RowElement} from "./interface";
+import * as CRUD from "./crud";
+import {insertRow} from "./crud";
+
+const row : RowElement = {
+    firstName: "Guillaume", lastName: "Salva"
+}
+
+// @ts-ignore
+const newRowId: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement ={
+    firstName: "Guillaume", lastName: "Salva", age: 23
+};
+CRUD.updateRow(newRowId, updatedRow);
+CRUD.deleteRow(newRowId);
